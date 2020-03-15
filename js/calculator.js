d2f09586d2db8b615c85d39fd72183e24b9357d0
Tim Kelly <timpkelly1@gmail.com>
2020-03-15 17:39:00 +0000

    Created the DOM layer selections for all buttons and added the event listeners relevant for each button

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,3 @@
-/* Things I need to create
-
-Creating functions for the following items and testing them in my browser's console
-1. add
-2. substract
-3. multiply
-4. divide
-*/
-
 function add(n1, n2) {
     return n1 + n2
 }
@@ -26,9 +17,6 @@ function divide(n1, n2) {
         return alert("You snake! If you divide by 0, you get nothing AND break the calculator")
     }
 }
-
-/* Create a new function operate() -> takes an operator an d2 numbers and 
-calls one of the above functions on numbers */
 
 function operate(n1, n2, operator) {
     switch(operator) {
@@ -51,7 +39,53 @@ function operate(n1, n2, operator) {
 }
 
 
-/* Create a function that populates the display when you click number buttons and stores display value */
+/* Create a function that populates the display when you click number buttons and stores display value 
+
+What this involves:
+0. Setting up a DOM layer variable that says displayContainer.InnerText = displayValue
+1. Setting up event listeners for number buttons
+2. Setting up event listeners for operator buttons
+3. Setting up event listeners for equal buttons
+4. Setting up event listeners for clear buttons
+5. For number clicks, get the number and add it to a displayValue variable ('string' + 'string') and update displayContainer.InnerText
+6. For operator clicks, store the old displayValue in n1 variable, get the operator value and store it in a operator variable 
+and reset the displayValue to 0 and update the displayContainer.InnerText
+7. For equal clicks, store the displayValue as n2 variable and set the new displayValue = operator(n1,n2,operator) and udpate the displayContainer.InnerText
+8. For clear clicks, make the variables n1, n2, operator and displayValue = 0 and update the displayContainer.InnerText
+*/
+
+const displayContainer = document.querySelector('.number-container')
+const numberBtns = document.querySelectorAll('.number-button')
+const operatorBtns = document.querySelectorAll('.operator-button:not(#equal)')
+const equalBtn = document.querySelector('#equal')
+const clearBtn = document.querySelector('.clear-button')
+
+// Testing the displayContainer
+displayContainer.textContent = 3223
+
+// Event listeners for calculator buttons
+numberBtns.forEach((button) => {
+
+    button.addEventListener('click', (e) => {
+      alert(button.textContent.replace(/\s/g, ""));
+    });
+});
+
+operatorBtns.forEach((button) => {
+
+button.addEventListener('click', (e) => {
+    alert(button.textContent.replace(/\s/g, ""));
+    });
+});
+
+equalBtn.addEventListener('click', function (e) {
+    alert(e.target.textContent.replace(/\s/g, ""));
+});
+
+clearBtn.addEventListener('click', function (e) {
+    alert(e.target.textContent.replace(/\s/g, ""));
+});
+
 
 /* Make the calc. work! Store first number n1 as an input when a user presses an operator
 and also save which operation has been chosen and then use the operate function when the user presses the = key */
